550f3f2d0d5e48ebf1e3e98373ff21b975bf6a8f
Anton Mishanin <antonmishanin@mail.ru>
2021-01-30 14:02:46 +0700

    Read ArrayList fun

---
+++ ArrayList/src/main/java/com/example/arraylist/ArrayListTraining.kt
@@ -0,0 +1,26 @@
+package com.example.arraylist
+
+import java.util.*
+import kotlin.collections.ArrayList
+
+class ArrayListTraining {
+
+    init {
+        init()
+    }
+
+    private fun init(){
+
+        val arrayList = ArrayList<String>(5)
+
+      // arrayList.add()
+      // arrayList.get()
+      // arrayList.addAll()
+
+      // arrayList.clear()
+      // arrayList.contains()
+      // arrayList.isEmpty()
+      // arrayList.remove()
+      // arrayList.set()
+    }
+}
