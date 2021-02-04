a050e0b8658616ea724f61930661163095c2aae6
Anton Mishanin <antonmishanin@mail.ru>
2021-02-04 20:47:00 +0700

    Read Set fun

---
+++ Set/src/main/java/com/example/set/SetTraining.kt
@@ -0,0 +1,20 @@
+package com.example.set
+
+import java.util.*
+import kotlin.collections.ArrayList
+import kotlin.collections.HashSet
+import kotlin.collections.LinkedHashSet
+
+class SetTraining {
+
+    init{
+        init()
+    }
+
+    private fun init(){
+      //HashSet<String>().contains()
+      //LinkedHashSet<String>()
+      //TreeSet<String>().add()
+      //ArrayList<String>().add()
+    }
+}
